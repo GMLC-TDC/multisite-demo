fd71fb5fb6aa78467d30bf66661396f3a1282577
Orestis Vasios <orestis.vasios@pnnl.gov>
2022-10-07 06:24:57 -0700

    Initial commit for transmission_dummy.m

---
+++ transmission/transmission_dummy.m
@@ -0,0 +1,10 @@
+% Author: Orestis Vasios (orestis.vasios@pnnl.gov)
+% Work in progress.
+% Last edit: 10/07/2022
+
+clear all
+clc
+
+%% Check if MATLAB or OCTAVE
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+
