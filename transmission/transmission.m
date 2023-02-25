38170f8d02a44227d5922193ebe13202f1367904
Orestis Vasios <orestis.vasios@pnnl.gov>
2023-02-24 16:58:11 -0800

    Formatting changes

--- transmission/transmission.m
+++ transmission/transmission.m
@@ -1,6 +1,6 @@
 % Author: Orestis Vasios (orestis.vasios@pnnl.gov)
 % Work in progress.
-% Last edit: 11/30/2022
+% Last edit: 02/24/2023
 
 clear all
 clear classes
@@ -8,8 +8,6 @@ clc
 
 %% Check if MATLAB or OCTAVE.
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-
-disp('Test!')
 
 %% Load Model
 % FIXME: Each user must use an appropriate file below that
@@ -32,7 +30,8 @@ if Wrapper.config_data.include_helics
     Wrapper = Wrapper.start_helics_federate('transmission_config.json');
 end
 
-disp('Test!')
+fprintf('Wrapper: Transmission federate started.\n')
+
 tnext_physics_powerflow = Wrapper.config_data.physics_powerflow.interval;
 tnext_real_time_market = Wrapper.config_data.real_time_market.interval;
 tnext_day_ahead_market = Wrapper.config_data.day_ahead_market.interval;
@@ -46,73 +45,73 @@ price_range = [10, 30];
 flexibility = 0.25;
 blocks = 10;
 
-disp('Test!')
 while time_granted <= Wrapper.duration
+    fprintf('Wrapper: Entered main execution loop.\n')
     next_helics_time =  min([tnext_physics_powerflow, tnext_real_time_market, tnext_day_ahead_market]);
     
     if Wrapper.config_data.include_helics
         time_granted  = helicsFederateRequestTime(Wrapper.helics_data.fed, next_helics_time);
-        fprintf('Wrapper: Requested  %ds in time and got Granted %d\n.', next_helics_time, time_granted)
+        fprintf('Wrapper: Requested  %ds in time and got Granted %d.\n', next_helics_time, time_granted)
     else
         time_granted = next_helics_time;
-        fprintf('Wrapper: Current Time %d\n.', time_granted)
+        fprintf('Wrapper: Current Time %d.\n', time_granted)
     end
 
-   if (time_granted >= tnext_real_time_market) && (Wrapper.config_data.include_real_time_market)
-            time_granted;
-            Wrapper = Wrapper.update_loads_from_profiles(time_granted, 'load_profile_info', 'load_profile');
+    if (time_granted >= tnext_real_time_market) && (Wrapper.config_data.include_real_time_market)
+        time_granted;
+        Wrapper = Wrapper.update_loads_from_profiles(time_granted, 'load_profile_info', 'load_profile');
             
-            % Collect Bids from DSO
-            if Wrapper.config_data.include_helics
-                Wrapper = Wrapper.get_bids_from_helics();
-            else
-                Wrapper = Wrapper.get_bids_from_cosimulation(time_granted, flexibility, price_range, blocks);
-            end
+        % Collect Bids from DSO
+        if Wrapper.config_data.include_helics
+            Wrapper = Wrapper.get_bids_from_helics();
+        else
+            Wrapper = Wrapper.get_bids_from_cosimulation(time_granted, flexibility, price_range, blocks);
+        end
             
-            %*************************************************************
-            Wrapper = Wrapper.run_RT_market(time_granted);
-            %***********************************************************
+        %*************************************************************
+        Wrapper = Wrapper.run_RT_market(time_granted);
+        %***********************************************************
             
-            %*************************************************************
-            % Collect Allocations from DSO
-            if Wrapper.config_data.include_helics
-                Wrapper = Wrapper.send_allocations_to_helics();
-            end
+        %*************************************************************
+        % Collect Allocations from DSO
+        if Wrapper.config_data.include_helics
+            Wrapper = Wrapper.send_allocations_to_helics();
+        end
             
-            if (Wrapper.config_data.include_helics) && (Wrapper.config_data.include_natural_gas)
-                Wrapper = Wrapper.send_requests_to_natural_gas_generators();
-            end
+        if (Wrapper.config_data.include_helics) && (Wrapper.config_data.include_natural_gas)
+            Wrapper = Wrapper.send_requests_to_natural_gas_generators();
+        end
             
-            tnext_real_time_market = tnext_real_time_market + Wrapper.config_data.real_time_market.interval;
+        tnext_real_time_market = tnext_real_time_market + Wrapper.config_data.real_time_market.interval;
     end
     
-     if (time_granted >= tnext_physics_powerflow) && (Wrapper.config_data.include_physics_powerflow)     
-             Wrapper = Wrapper.update_loads_from_profiles(time_granted, 'load_profile_info', 'load_profile');
+    if (time_granted >= tnext_physics_powerflow) && (Wrapper.config_data.include_physics_powerflow)     
+        Wrapper = Wrapper.update_loads_from_profiles(time_granted, 'load_profile_info', 'load_profile');
              
-             % Collect measurements from distribution networks
-             if Wrapper.config_data.include_helics  
-                 Wrapper = Wrapper.get_loads_from_helics();
-                 % FIXME: Add code for other dummy federates: Start
-                 if (Wrapper.config_data.include_helics) && (Wrapper.config_data.include_natural_gas)
-                     Wrapper = Wrapper.get_generation_from_natural_gas_generators();
-                 end
-                 % FIXME: Add code for other dummy federates: End                 
-             end
-             %*************************************************************
-             Wrapper = Wrapper.run_power_flow(time_granted);  
-             %*************************************************************
-             % Send Voltages from distribution networks
-             if Wrapper.config_data.include_helics  
-                 Wrapper = Wrapper.send_voltages_to_helics();
-                 % FIXME: Add code for other dummy federates: Start
-                 % FIXME: Add code for other dummy federates: End                 
-             end
+        % Collect measurements from distribution networks
+        if Wrapper.config_data.include_helics  
+            Wrapper = Wrapper.get_loads_from_helics();
+            % FIXME: Add code for other dummy federates: Start
+            if (Wrapper.config_data.include_helics) && (Wrapper.config_data.include_natural_gas)
+                Wrapper = Wrapper.get_generation_from_natural_gas_generators();
+            end
+            % FIXME: Add code for other dummy federates: End                 
+        end
+        %*************************************************************
+        Wrapper = Wrapper.run_power_flow(time_granted);  
+        %*************************************************************
+        % Send Voltages from distribution networks
+        if Wrapper.config_data.include_helics  
+            Wrapper = Wrapper.send_voltages_to_helics();
+            % FIXME: Add code for other dummy federates: Start
+            % FIXME: Add code for other dummy federates: End                 
+        end
  
-             tnext_physics_powerflow = tnext_physics_powerflow + Wrapper.config_data.physics_powerflow.interval;
-     end
+        tnext_physics_powerflow = tnext_physics_powerflow + Wrapper.config_data.physics_powerflow.interval;
+    end
 
     if time_granted == Wrapper.duration     %end infinite loop
-        time_granted = Wrapper.duration+1;
+        time_granted = Wrapper.duration + 1;
     end
 end
 
